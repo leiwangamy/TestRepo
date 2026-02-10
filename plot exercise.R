3d0be6dae37ee8360edc411805afa583584c3ac6
Lei Wang <206961662+leiwangamy@users.noreply.github.com>
2026-02-10 14:02:48 -0800

    Add files via upload

---
+++ plot exercise.R
@@ -0,0 +1,33 @@
+
+library(datasets)
+
+#Load Data
+
+data(mtcars)
+
+#View first 5 rows
+
+head(mtcars, 5)
+
+install.packages("ggplot2")
+
+library(ggplot2)
+
+ggplot(aes(x = disp, y = mpg), data = mtcars) +
+  geom_point()
+#Add a title
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon")
+
+#change axis name
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon") + labs(x = "Displacement", y = "Miles per Gallon")
+
+#make vs a factor
+mtcars$vs <- as.factor(mtcars$vs)
+
+#create boxplot of the distribution for v-shaped and straight Engine
+
+ggplot(aes(x=vs, y=mpg), data = mtcars) + geom_boxplot()
+
+ggplot(aes(x=wt),data=mtcars) + geom_histogram(binwidth=0.5)
